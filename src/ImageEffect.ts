3a1fcbb0ccd3e728fcff7f484ae09364cf30728c
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2023-12-28 15:15:03 +0100

    added additional params

--- src/ImageEffect.ts
+++ src/ImageEffect.ts
@@ -279,6 +279,8 @@ export class ImageEffect implements EffectRenderer {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filter);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filter);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     this.cacheSet(gl, cacheKey, texture);
   }
